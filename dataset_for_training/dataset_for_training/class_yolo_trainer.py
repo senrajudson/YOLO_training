a954faf58595cefe942dfda257935dbe79fbc60f
senra.judson <igor.santos@aperam.com>
2023-11-22 14:54:13 -0300

    YOLO archives copy

--- dataset_for_training/dataset_for_training/class_yolo_trainer.py
+++ dataset_for_training/dataset_for_training/class_yolo_trainer.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from ultralytics import YOLO
 
 class YOLOTrainer:
 
@@ -7,6 +8,34 @@ class YOLOTrainer:
         self._image_folder_origin = None
         self._annotations_folder_origin = None
         self._test_percentual_divisor = None
+        self._predict_YOLO = None
+        self._yolo_Classes = None
+        self._yolo_Notes = None
+
+    @property
+    def yolo_Classes(self):
+        return self._yolo_Classes
+    
+    @yolo_Classes.setter
+    def yolo_Classes(self, path):
+        self._yolo_Classes = path
+
+    @property
+    def yolo_Notes(self):
+        return self._yolo_Notes
+    
+    @yolo_Notes.setter
+    def yolo_Notes(self, path):
+        self._yolo_Notes = path
+
+    
+    @property
+    def predict_YOLO(self):
+        return self._predict_YOLO
+    
+    @predict_YOLO.setter
+    def predict_YOLO(self, predict_object):
+        self._predict_YOLO = predict_object
 
     @property
     def annotations_folder_origin(self):
@@ -40,6 +69,16 @@ class YOLOTrainer:
     def slicing_dataset_for_traning(self):
 
         list_archives = [self.image_folder_origin, self.annotations_folder_origin]
+        yolo_dataset_dir = "YOLO"
+        os.makedirs(yolo_dataset_dir, exist_ok=True)
+
+        if self._yolo_Classes:
+            yolo_classes_path = os.path.join(self._yolo_Classes, "classes.txt")
+            shutil.copy(yolo_classes_path, yolo_dataset_dir)
+
+        if self._yolo_Notes:
+            yolo_notes_path = os.path.join(self._yolo_Notes, "notes.json")
+            shutil.copy(yolo_notes_path, yolo_dataset_dir)
 
         for folder in list_archives:
             if folder != None:
@@ -61,4 +100,11 @@ class YOLOTrainer:
                         
                         path_destination = os.path.join(destination, file)
                         shutil.copy(path_file, path_destination)
+
+    def training_YOLO_model(self):
+        model = YOLO("yolov8n.pt")
+        results = model.train(data = "././YOLO", epochs=5, imsz=640)
+
+        return results
+
 
